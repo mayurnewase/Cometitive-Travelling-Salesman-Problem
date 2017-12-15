abc7ec3c08d2a7ef1928315f5349de5e62b8955c
Mayur Newase <mayurnewase111@gmail.com>
2017-12-15 07:38:56 +0530

    Update main_gui.py
    Added connectivity between cities.

--- main_gui.py
+++ main_gui.py
@@ -7,14 +7,14 @@ csv_file_path = "distanceFile.csv"
 df = pd.read_csv(csv_file_path)
 X_pos = df.x_pos
 Y_pos = df.y_pos
-
+id = df.id
 
 master = Tk()
 w = Canvas(master , width = 600 , height = 600)
 w.pack()
 
 
-def drawCircle(x , y , r = 5):
+def drawCircle(x , y , r = 9):
 	id = w.create_oval(x-r , y-r , x+r , y+r , fill = "blue")
 	return id
 
@@ -31,38 +31,20 @@ def drawAxis(width , height):
 		w.create_text(13 , i , text = i)
 		i = i + 50
 
-
-
 drawAxis(600 , 600)
 
-for i in range (9):
+for i in range (11):
 	drawCircle(X_pos[i] , Y_pos[i])
 
-w.create_line(X_pos[1] , Y_pos[1] , X_pos[5] , Y_pos[5] , fill = "red" , width = 2)
 
+def drawConnectivity(city1_X , city1_Y , city2_X , city2_Y):
+	w.create_line(city1_X , city1_Y , city2_X , city2_Y , width = 2 , fill = "red")
+
+
+for i in id:
+	j=i+1
+	while (j <11):
+		drawConnectivity(X_pos[i] , Y_pos[i] , X_pos[j] , Y_pos[j])
+		j += 1
 
 mainloop()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
